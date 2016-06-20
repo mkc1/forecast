2166f18b55f2e43adfba89cbbea6ead8cb0d85b3
mkc1 <coulism@gmail.com>
2016-06-20 14:02:25 -0400

    update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function findCoordinates(){
         var jsonResponse = JSON.parse(xhr.responseText);
         var latitude = jsonResponse.results[0].geometry.location.lat;
         var longitude = jsonResponse.results[0].geometry.location.lng;
-        getforecast();
+        makeCorsRequest();
       } else {
        
       console.log('Error: ' + xhr.status);
@@ -77,13 +77,13 @@ function makeCorsRequest() {
 
 }
 
-function foo(data) 
-  {
-    console.log('hellooo')
-    console.log(data)
-  }
+// function foo(data) 
+//   {
+//     console.log('hellooo')
+//     console.log(data)
+//   }
 
-  var script = document.createElement('script');
-  script.src = 'https://api.forecast.io/forecast/6942987057d91c10a7960fdcf9b0d96e/37.8267,-122.423?callback=foo()'
+//   var script = document.createElement('script');
+//   script.src = 'https://api.forecast.io/forecast/6942987057d91c10a7960fdcf9b0d96e/37.8267,-122.423?callback=foo()'
 
-  document.head.appendChild(script);
+//   document.head.appendChild(script);
