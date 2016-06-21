370021832a4824bf8b68656078c9c08975be751d
mkc1 <coulism@gmail.com>
2016-06-21 09:18:36 -0400

    added imgs

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function findCoordinates(){
       if (xhr.status === 200) {
         console.log(jsonResponse);
         var jsonResponse = JSON.parse(xhr.responseText);
+        var city = jsonResponse()s
         var latitude = jsonResponse.results[0].geometry.location.lat;
         var longitude = jsonResponse.results[0].geometry.location.lng;
         var script = document.createElement('script');
@@ -33,43 +34,43 @@ function createForecastTables(data) {
   hourly(data);
 }
 
-  function weekly(myData) {
-    var weeklyForecast = ['<caption>Weekly Forecast</caption>', '<tr><td>Day</td><td>Temp (hi/low)</td><td>Feels like (hi/low)</td><td>Summary</td></tr>'];
-    for (var i = 0; i < 7; i++) {
-      var date = new Date(myData.daily.data[i].time*1000);
-      var newLine = "<tr><td>" + date.toString().substring(0, 10) + "</td>";
-      newLine += "<td>" + myData.daily.data[i].temperatureMax  + " / " +  myData.daily.data[i].temperatureMin + "</td>";
-      newLine += "<td>" + myData.daily.data[i].apparentTemperatureMax + " / " + myData.daily.data[i].apparentTemperatureMin + "</td>";
-      newLine += "<td>" + myData.daily.data[i].summary + "</td></tr>";
-      weeklyForecast.push(newLine);
-    }
-    var HTMLstring = weeklyForecast.join('')
-    document.getElementById('week').innerHTML = HTMLstring;
+function weekly(myData) {
+  var weeklyForecast = ['<caption>Weekly Forecast</caption>', '<tr><th>Day</th><th>Temp (hi/low)</th><th>Feels like (hi/low)</th><th>Summary</th></tr>'];
+  for (var i = 0; i < 7; i++) {
+    var date = new Date(myData.daily.data[i].time*1000);
+    var newLine = "<tr><td>" + date.toString().substring(0, 10) + "</td>";
+    newLine += "<td>" + Math.round(myData.daily.data[i].temperatureMax)  + " / " +  Math.round(myData.daily.data[i].temperatureMin) + "</td>";
+    newLine += "<td>" + Math.round(myData.daily.data[i].apparentTemperatureMax) + " / " + Math.round(myData.daily.data[i].apparentTemperatureMin) + "</td>";
+    newLine += "<td>" + myData.daily.data[i].summary + "</td></tr>";
+    weeklyForecast.push(newLine);
   }
+  var HTMLstring = weeklyForecast.join('')
+  document.getElementById('week').innerHTML = HTMLstring;
+}
 
-  function hourly(myData) {
-    var hourlyForecast = ['<caption>Next 12 Hours</caption>', '<tr><td>Time</td><td>Temp</td><td>Feels like</td><td>Summary</td></tr>'];
-    for (var i = 0; i < 13; i++) {
-      var date = new Date(myData.hourly.data[i].time*1000);
-      var time = formatTime(date);
-      var newLine = "<tr><td>" + time + "</td>";
-      newLine += "<td>" + myData.hourly.data[i].temperature + "</td>";
-      newLine += "<td>" + myData.hourly.data[i].apparentTemperature + "</td>";
-      newLine += "<td>" + myData.hourly.data[i].summary + "</td></tr>";
-      hourlyForecast.push(newLine);
-    }
-    var HTMLstring = hourlyForecast.join('')
-    console.log(HTMLstring)
-    document.getElementById('today').innerHTML = HTMLstring;
+function hourly(myData) {
+  var hourlyForecast = ['<caption>Next 12 Hours</caption>', '<tr><td>Time</td><td>Temp</td><td>Feels like</td><td>Summary</td></tr>'];
+  for (var i = 0; i < 12; i++) {
+    var date = new Date(myData.hourly.data[i].time*1000);
+    var time = formatTime(date);
+    var newLine = "<tr><td>" + time + "</td>";
+    newLine += "<td>" + Math.round(myData.hourly.data[i].temperature) + "</td>";
+    newLine += "<td>" + Math.round(myData.hourly.data[i].apparentTemperature) + "</td>";
+    newLine += "<td>" + myData.hourly.data[i].summary + "</td></tr>";
+    hourlyForecast.push(newLine);
   }
+  var HTMLstring = hourlyForecast.join('')
+  console.log(HTMLstring)
+  document.getElementById('today').innerHTML = HTMLstring;
+}
 
-  function formatTime(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
+function formatTime(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
 }
