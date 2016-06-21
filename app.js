821ce5bc404d68bdf0c7ebb23eb9285b17418d0c
mkc1 <coulism@gmail.com>
2016-06-21 10:22:21 -0400

    city

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function findCoordinates(){
         script.src = 'https://api.forecast.io/forecast/6942987057d91c10a7960fdcf9b0d96e/' + latitude + ',' + longitude + '?callback=createForecastTables'
 
         document.head.appendChild(script);
-        ocument.getElementsByClassName("city").innerHTML = city;
+        document.getElementsByClassName("city").innerHTML = city;
         createForecastTables();
 
       } else {
